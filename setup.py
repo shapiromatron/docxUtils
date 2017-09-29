35dc82ee269c100316e988ece4d8576071cd0305
Andy Shapiro <shapiromatron@gmail.com>
2017-09-29 11:04:01 -0400

    setup py.test and tox for py27/py36

--- setup.py
+++ setup.py
@@ -1,22 +1,30 @@
 from setuptools import setup
+import re
 
-version = '0.0.1'
 
-setup(name='docxUtils',
-      version=version,
-      description='Utility functions for python-docx library',
-      long_description=open('README.rst').read(),
-      classifiers=[
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.2',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-      ],
-      author='Andy Shapiro',
-      author_email='shapiromatron@gmail.com',
-      url='http://github.com/shapiromatron/docxUtils',
-      license='MIT',
-      packages=['docxUtils'],
-      install_requires=['python-docx>=0.8.5']
+def get_version():
+    regex = r"""^__version__ = '(.*)'$"""
+    with open('docxUtils/__init__.py', 'r') as f:
+        text = f.read()
+    return re.findall(regex, text, re.MULTILINE)[0]
+
+
+setup(
+    name='docxUtils',
+    version=get_version(),
+    description='Utility functions for python-docx library',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+    ],
+    author='Andy Shapiro',
+    author_email='shapiromatron@gmail.com',
+    url='http://github.com/shapiromatron/docxUtils',
+    license='MIT',
+    packages=['docxUtils'],
+    install_requires=['python-docx>=0.8.6'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
 )
---
+++ tests/reports.py
@@ -0,0 +1,15 @@
+import os
+from docxUtils import DOCXReport
+
+
+class ReporterTest(DOCXReport):
+
+    def create_content(self):
+        self.doc.add_heading('Hello {}'.format(self.context['name']), level=1)
+
+
+def test_reporter():
+    root_path = os.path.expanduser('~/Desktop/report.docx')
+    report = ReporterTest(None, {'name': 'Moto'})
+    docx = report.build_report()
+    report.save_report(docx, root_path)
